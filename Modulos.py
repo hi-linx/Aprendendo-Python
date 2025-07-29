9085187a0f199c279dc0abcc7e0ae1b0f69fc8e6
lucas soares <168036085+hi-linx@users.noreply.github.com>
2025-07-29 20:40:26 -0300

    Add files via upload

---
+++ Modulos.py
@@ -0,0 +1,18 @@
+#import math -> importa tudo da biblioteca math
+#from math import sqrt -> importa somente o elemento sqrt da biblioteca math
+#from math import sqrt, ceil -> importando mais de um elemento, mas n a biblioteca inteira
+
+#import math
+#num = int(input('Digite um número: '))
+#raiz = math.sqrt(num)
+
+#print(f'A raiz de {num} é {raiz:.3f}')
+
+#from math import sqrt
+#num = int(input('Digite um número: '))
+#raiz = sqrt(num)
+
+#print(f'A raiz de {num} é {raiz:.3f}')
+
+import random
+num = random.randint(1,10) #randint -> vai gerar um numero aleatorio inteiro
